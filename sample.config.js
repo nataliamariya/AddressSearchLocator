72c45aac2c20b49a40f2d5430cf41ee3cd9a3cb2
nataliamariya <natalia.bohulevych@gmail.com>
2023-05-08 00:21:18 -0400

    fixed up formatting

--- sample.config.js
+++ sample.config.js
@@ -1,5 +1,7 @@
+// sample.config.js
 
 // Before running this website, please make a file 
 // called config.js with the following code:
 
-window.apiKey = "YOUR_API_KEY_HERE"; //YOUR_API_KEY_HERE is to be replaced with your actual API key
+window.apiKey = "YOUR_API_KEY_HERE"; 
+//YOUR_API_KEY_HERE is to be replaced with your actual API key
