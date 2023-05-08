fbbb4115756535c0254b3fb590b05315ae7f4662
nataliamariya <natalia.bohulevych@gmail.com>
2023-05-07 23:57:29 -0400

    update sample.config.js

--- sample.config.js
+++ sample.config.js
@@ -1,5 +1,5 @@
 
-Before running this website, please make a file 
-called config.js with the following code:
+// Before running this website, please make a file 
+// called config.js with the following code:
 
 window.apiKey = "YOUR_API_KEY_HERE"; //YOUR_API_KEY_HERE is to be replaced with your actual API key
